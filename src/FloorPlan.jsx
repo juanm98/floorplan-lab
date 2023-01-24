6cc83104730d538fd4fc40f98bf11af2d1150a70
Juan Garcia <juanmgarcia098@gmail.com>
2023-01-24 09:33:33 -0400

    Add bedroom and bath to app.js

--- src/FloorPlan.jsx
+++ src/FloorPlan.jsx
@@ -1,11 +1,18 @@
 import Kitchen from './Kitchen';
 import LivingRoom from './LivingRoom';
+import Bath from './Bath';
+import Bedroom from './Bedroom';
 
 const FloorPlan = (props) => {
   return ( 
     <div>
+      <Bedroom bedNum={1}/>
       <Kitchen/>
+      <Bath size={"Full"}/>
+      <Bedroom bedNum={2}/>
       <LivingRoom/>
+      <Bath size={"Half"}/>
+      <Bedroom bedNum={3}/>
     </div>
   );
 }
